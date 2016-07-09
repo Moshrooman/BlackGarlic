e09d95eba884c31938a1d4630a21c9597a300599
Moshrooman <justinkwik@blackgarlic.id>
2016-07-09 22:23:01 +0700

    scale animation for dots done

--- app/src/main/java/id/blackgarlic/blackgarlic/CookBookModel/CookBookClicked.java
+++ app/src/main/java/id/blackgarlic/blackgarlic/CookBookModel/CookBookClicked.java
@@ -2,18 +2,14 @@ package id.blackgarlic.blackgarlic.CookBookModel;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import id.blackgarlic.blackgarlic.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -26,6 +22,7 @@ public class CookBookClicked extends AppCompatActivity implements ViewPager.OnPa
     private static CookBookViewPagerAdapter viewPagerAdapter;
     private static int dotCount;
     private static ImageView[] dotsImageViewArray;
+    private static int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +33,7 @@ public class CookBookClicked extends AppCompatActivity implements ViewPager.OnPa
         dotsLinearLayout = (LinearLayout) findViewById(R.id.dotsLinearLayout);
         viewPagerTitle = (TextView) findViewById(R.id.viewPagerTitle);
         viewPagerAdapter = new CookBookViewPagerAdapter(CookBookClicked.this);
+        currentPosition = 0;
 
         cookBookViewPager.setAdapter(viewPagerAdapter);
         cookBookViewPager.setCurrentItem(0);
@@ -54,7 +52,7 @@ public class CookBookClicked extends AppCompatActivity implements ViewPager.OnPa
             if (i == 0) {
                 params.setMargins(0, 0, 0, 0);
             } else {
-                params.setMargins(4, 0, 0, 0);
+                params.setMargins(15, 0, 0, 0);
             }
 
             dotsLinearLayout.addView(dotsImageViewArray[i], params);
@@ -72,11 +70,36 @@ public class CookBookClicked extends AppCompatActivity implements ViewPager.OnPa
     @Override
     public void onPageSelected(int position) {
 
+        Animation upscaleDotLeftToRight = AnimationUtils.loadAnimation(CookBookClicked.this, R.anim.anim_upscale_dot_lefttoright);
+        Animation upscaleDotRightToLeft = AnimationUtils.loadAnimation(CookBookClicked.this, R.anim.anim_upscale_dot_righttoleft);
+
+        Animation downscaleDotLeftToRight = AnimationUtils.loadAnimation(CookBookClicked.this, R.anim.anim_downscale_dot_lefttoright);
+        Animation downscaleDotRightToLeft = AnimationUtils.loadAnimation(CookBookClicked.this, R.anim.anim_downscale_dot_righttoleft);
+
         for (int i = 0; i < dotsImageViewArray.length; i++) {
-            dotsImageViewArray[i].setImageDrawable(getResources().getDrawable(R.drawable.nonselecteddot));
+
+            if (dotsImageViewArray[i].getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.selecteddot).getConstantState())) {
+                dotsImageViewArray[i].setImageDrawable(getResources().getDrawable(R.drawable.nonselecteddot));
+
+                if (position > currentPosition) {
+                    dotsImageViewArray[i].startAnimation(downscaleDotLeftToRight);
+                } else {
+                    dotsImageViewArray[i].startAnimation(downscaleDotRightToLeft);
+                }
+
+            }
+
         }
 
         dotsImageViewArray[position].setImageDrawable(getResources().getDrawable(R.drawable.selecteddot));
+
+        if (position > currentPosition) {
+            dotsImageViewArray[position].startAnimation(upscaleDotRightToLeft);
+            currentPosition = position;
+        } else {
+            dotsImageViewArray[position].startAnimation(upscaleDotLeftToRight);
+            currentPosition = position;
+        }
 
     }
 
