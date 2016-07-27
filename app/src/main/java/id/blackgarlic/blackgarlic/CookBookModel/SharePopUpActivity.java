d5461a95cf3f68ec1dca8a18b0a809241a26f4f5
Moshrooman <justinkwik@blackgarlic.id>
2016-07-27 13:14:36 +0700

    sharing work in progress

--- app/src/main/java/id/blackgarlic/blackgarlic/CookBookModel/SharePopUpActivity.java
+++ app/src/main/java/id/blackgarlic/blackgarlic/CookBookModel/SharePopUpActivity.java
@@ -3,23 +3,34 @@ package id.blackgarlic.blackgarlic.CookBookModel;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.common.executors.CallerThreadExecutor;
 import com.facebook.common.references.CloseableReference;
 import com.facebook.datasource.DataSource;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.generic.RoundingParams;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
@@ -32,6 +43,9 @@ import com.facebook.share.ShareApi;
 import com.facebook.share.Sharer;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+import com.github.johnpersano.supertoasts.SuperToast;
+import com.github.johnpersano.supertoasts.util.Style;
 import com.google.gson.Gson;
 
 import java.util.Arrays;
@@ -49,6 +63,10 @@ public class SharePopUpActivity extends AppCompatActivity {
     private static CallbackManager callbackManager;
     private static LoginManager loginManager;
     private static Bitmap imageToShare;
+    private static FacebookCallback<Sharer.Result> facebookCallback;
+    private static ImageView confirmImage;
+    private static EditText pictureDescriptionEditText;
+    private static Button postButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,9 +80,31 @@ public class SharePopUpActivity extends AppCompatActivity {
         cookBookObject = new Gson().fromJson(getIntent().getExtras().getString("object", ""), CookBookObject.class);
         sharePopUpRelativeLayout = (RelativeLayout) findViewById(R.id.sharePopUpRelativeLayout);
         faceBookImage = (SimpleDraweeView) findViewById(R.id.faceBookImage);
+        confirmImage = (ImageView) findViewById(R.id.confirmImage);
+        pictureDescriptionEditText = (EditText) findViewById(R.id.pictureDescriptionEditText);
+        postButton = (Button) findViewById(R.id.postButton);
         callbackManager = CallbackManager.Factory.create();
         final List<String> permissions = Arrays.asList("publish_actions");
         loginManager = LoginManager.getInstance();
+        facebookCallback = new FacebookCallback<Sharer.Result>() {
+            @Override
+            public void onSuccess(Sharer.Result result) {
+                Log.e("Facebook Callback: ", String.valueOf(result.getPostId()));
+                SuperToast superToast = SuperToast.create(SharePopUpActivity.this, "Successfully Posted Picture!", SuperToast.Duration.MEDIUM, Style.getStyle(Style.BLUE, SuperToast.Animations.FLYIN));
+                superToast.show();
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("Facebook Callback: ", "Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("Facebook Callback: ", error.getCause().toString());
+            }
+        };
 
         sharePopUpRelativeLayout.post(new Runnable() {
             @Override
@@ -82,19 +122,48 @@ public class SharePopUpActivity extends AppCompatActivity {
                 loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        publishImage();
+                        Log.e("CallBack: ", "Success");
+                        RoundingParams roundingParams = RoundingParams.fromCornersRadius(5f);
+                        roundingParams.setBorder(R.color.red, 4);
+                        roundingParams.setRoundAsCircle(false);
+                        faceBookImage.getHierarchy().setRoundingParams(roundingParams);
+                        confirmImage.setVisibility(View.VISIBLE);
+                        pictureDescriptionEditText.setVisibility(View.VISIBLE);
+                        postButton.setVisibility(View.VISIBLE);
+
+                        getWindow().setLayout(sharePopUpRelativeLayout.getWidth(), 1500);
+
+                        setConfirmImageAndBitmap();
                     }
 
                     @Override
                     public void onCancel() {
-
+                        Log.e("CallBack: ", "Cancelled");
                     }
 
                     @Override
                     public void onError(FacebookException error) {
-
+                        Log.e("CallBack: ", "Error");
+                        Log.e("Error: ", error.getCause().toString());
                     }
                 });
+            }
+        });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setBitmap(imageToShare)
+                        .setCaption(pictureDescriptionEditText.getText().toString())
+                        .build();
+
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(photo)
+                        .setContentUrl(Uri.parse("www.blackgarlic.id"))
+                        .build();
+
+                ShareApi.share(content, facebookCallback);
             }
         });
     }
@@ -104,7 +173,7 @@ public class SharePopUpActivity extends AppCompatActivity {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
-    public void publishImage() {
+    public void setConfirmImageAndBitmap() {
 
         Uri cookBookImageUri = Uri.parse(BLACKGARLIC_PICTURES.replace("menu_id", String.valueOf(cookBookObject.getMenu_id())));
 
@@ -122,6 +191,7 @@ public class SharePopUpActivity extends AppCompatActivity {
             protected void onNewResultImpl(Bitmap bitmap) {
 
                 imageToShare = bitmap;
+                confirmImage.setImageBitmap(bitmap);
 
             }
 
@@ -131,22 +201,26 @@ public class SharePopUpActivity extends AppCompatActivity {
             }
         }, CallerThreadExecutor.getInstance());
 
-        SharePhoto photo = new SharePhoto.Builder()
-                .setBitmap(imageToShare)
-                .setCaption("Awesome Menu From BlackGarlic!")
-                .build();
-
-        SharePhotoContent content = new SharePhotoContent.Builder()
-                .addPhoto(photo)
-                .build();
-
-        ShareApi.share(content, (FacebookCallback<Sharer.Result>) callbackManager);
         }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        AppEventsLogger.deactivateApp(this);
     }
 }
 
